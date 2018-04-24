dde7a1d18d709ac9c046fd800dd3fdaadeb82806
omikron15 <omikron15@hotmail.co.uk>
2018-04-24 11:36:33 +0100

    Scroll bar functionality added to main page

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,22 +13,22 @@ artist1 = Artist.new({
 
 artist2 = Artist.new({
   "name" => "Artist 2",
-  "artist_picture" => "./images/bass-guitar-chord-close-up-96380.jpg"
+  "artist_picture" => "./images/abstract-art-blur-417458.jpg"
 })
 
 artist3 = Artist.new({
   "name" => "Artist 3",
-  "artist_picture" => "./images/bass-guitar-chord-close-up-96380.jpg"
+  "artist_picture" => "./images/acoustic-antique-art-290660.jpg"
 })
 
 artist4 = Artist.new({
   "name" => "Artist 4",
-  "artist_picture" => "./images/bass-guitar-chord-close-up-96380.jpg"
+  "artist_picture" => "./images/adult-beautiful-blur-374703.jpg"
 })
 
 artist5 = Artist.new({
   "name" => "Artist 5",
-  "artist_picture" => "./images/bass-guitar-chord-close-up-96380.jpg"
+  "artist_picture" => "./images/audience-backlit-band-154147.jpg"
 })
 
 artist1.save
@@ -42,7 +42,7 @@ album1 = Album.new({
   "name" => "Album 1",
   "artist_id" => artist1.id,
   "genre" => "Rock",
-  "stock_leve" => 0,
+  "stock_leve" => 11,
   "album_picture" => "./images/album-black-classic-167092.jpg"
 })
 
@@ -50,48 +50,48 @@ album2 = Album.new({
   "name" => "Album 2",
   "artist_id" => artist2.id,
   "genre" => "Pop",
-  "stock_leve" => 0,
-  "album_picture" => "./images/album-black-classic-167092.jpg"
+  "stock_leve" => 9,
+  "album_picture" => "./images/audience-band-concert-167491.jpg"
 })
 
 album3 = Album.new({
   "name" => "Album 3",
   "artist_id" => artist3.id,
   "genre" => "Indy",
-  "stock_leve" => 0,
-  "album_picture" => "./images/album-black-classic-167092.jpg"
+  "stock_leve" => 3,
+  "album_picture" => "./images/audience-band-concert-167636.jpg"
 })
 
 album4 = Album.new({
   "name" => "Album 4",
   "artist_id" => artist4.id,
   "genre" => "Pop",
-  "stock_leve" => 0,
-  "album_picture" => "./images/album-black-classic-167092.jpg"
+  "stock_leve" => 30,
+  "album_picture" => "./images/bass-guitar-chord-close-up-96380.jpg"
 })
 
 album5 = Album.new({
   "name" => "Album 5",
   "artist_id" => artist5.id,
   "genre" => "Metal",
-  "stock_leve" => 0,
-  "album_picture" => "./images/album-black-classic-167092.jpg"
+  "stock_leve" => 1,
+  "album_picture" => "./images/close-up-fashion-female-373945.jpg"
 })
 
 album6 = Album.new({
   "name" => "Album 6",
   "artist_id" => artist1.id,
   "genre" => "Country",
-  "stock_leve" => 0,
-  "album_picture" => "./images/album-black-classic-167092.jpg"
+  "stock_leve" => 5,
+  "album_picture" => "./images/concert-music-musical-instrument-111287.jpg"
 })
 
 album7 = Album.new({
   "name" => "Album 7",
   "artist_id" => artist5.id,
   "genre" => "Indy",
-  "stock_leve" => 0,
-  "album_picture" => "./images/album-black-classic-167092.jpg"
+  "stock_leve" => 20,
+  "album_picture" => "./images/earphones-earpods-justifyyourlove-802958.jpg"
 })
 
 album1.save
